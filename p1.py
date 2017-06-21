2445e7985b706db91226098d66d9de39786b4b66
JSeyedalifathima1997 <seyedalifathima962@gmail.com>
2017-06-21 11:29:02 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,11 @@
+
+a = list()
+num =input("Enter how many elements you want:")
+print 'Enter numbers in array: '
+for i in range(int(num)):
+    n =input("num :")
+    a.append(int(n))
+print(a)
+
+	b=sum(a)
+	print(b)
